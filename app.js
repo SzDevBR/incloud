e1414589065ce7cc6b60301b94e097a5a41b1284
SzDevBR <81070056+SzDevBR@users.noreply.github.com>
2023-07-30 14:04:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -16,20 +16,16 @@ app.set('view engine', 'ejs');
 // Definir a pasta de visualizações
 app.set('views', path.join(__dirname, 'views'));
 
-// Exemplo de renderização da página "dashboard.ejs"
 app.get('/dashboard', (req, res) => {
-  const user = req.user; // Assume que o objeto do usuário foi corretamente armazenado na variável 'user'
-
-  // Acesso às informações do usuário
+  const user = req.user;
   const avatarUrl = user.avatar; // URL do avatar (foto) do usuário
   const username = user.username; // Nome de usuário do usuário
   const discriminator = user.discriminator; // Discriminador do usuário (os quatro dígitos após o nome de usuário, por exemplo, #1234)
 
   // Outras lógicas para obter os dados da aplicação do usuário
-
-  // Aqui você deve passar as informações para a view "dashboard"
-res.render('dashboard', { user: user, avatar: avatarUrl, username: username, discriminator: discriminator });
+  res.render('dashboard', { user: user, avatar: avatarUrl, username: username, discriminator: discriminator });
 });
+
 app.get('/upload', (req, res) => {
   const user = req.user;
   // Acesso às informações do usuário
